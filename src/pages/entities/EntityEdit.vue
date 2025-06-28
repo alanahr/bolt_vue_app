264cc899f71b89e60ec954eca2c7f2761b599947
AAR <25590997+alanahr@users.noreply.github.com>
2025-06-28 03:49:26 -0500

    Updated EntityEdit.vue

--- src/pages/entities/EntityEdit.vue
+++ src/pages/entities/EntityEdit.vue
@@ -108,7 +108,7 @@ const handleSubmit = async () => {
             v-for="entity_parent in filteredEntities"
             :key="entity_parent.id"
             class="list-group-item list-group-item-action"
-            @click="addParentEntity(entity_parent.id)"
+            @click.stop="addParentEntity(entity_parent.id)"
           >
             {{ entity_parent.name }}
           </button>
