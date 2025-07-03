0948244750366377ae1bdbdb0e65984c834fb1ab
AAR <25590997+alanahr@users.noreply.github.com>
2025-07-02 21:15:23 -0500

    Updated EntityList.vue

--- src/pages/entities/EntityList.vue
+++ src/pages/entities/EntityList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useEntityStore } from '../../stores/entities'
 import type { Entity } from '../../types/entity'
 
