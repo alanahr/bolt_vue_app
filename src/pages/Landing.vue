993ef50aa075c308af521f7a5b6410776e413c87
AAR <25590997+alanahr@users.noreply.github.com>
2025-07-04 00:46:11 -0500

    Updated Landing.vue

---
+++ src/pages/Landing.vue
@@ -0,0 +1,8 @@
+
+<template>
+  <div class="jumbotron">
+    <div class="card"> 
+      <router-link to="/entities/new" class="btn btn-success">Create New Entity</router-link>
+    </div>
+  </div>
+</template>
