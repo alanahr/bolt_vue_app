728572aef63f39557f6b073ab0d959b39051b7cf
AAR <25590997+alanahr@users.noreply.github.com>
2025-07-04 00:36:09 -0500

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-    <div class="container">
-      <router-link class="navbar-brand" to="/">Vue CRUD App</router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/entities">Entities</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/positions">Positions</router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <AppNavar />
 
   <div class="container">
     <router-view></router-view>
