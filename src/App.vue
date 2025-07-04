a8629218acb985b96d676ea593dc470a9a34c7fa
AAR <25590997+alanahr@users.noreply.github.com>
2025-07-04 00:33:00 -0500

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,12 @@
   </div>
 </template>
 
+<script setup lang="ts">
+import AppNavbar from './components/layout/AppNavbar.vue'
+import AppFooter from './components/layout/AppFooter.vue'
+import AlertBar from './components/layout/AlertBar.vue';
+</script>
+
 <style>
 #app {
   width: 100%;
