a695a946f5d0c8d7876c0c41c665a0e526f22975
AAR <25590997+alanahr@users.noreply.github.com>
2025-07-04 00:47:28 -0500

    Updated Landing.vue

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -2,7 +2,9 @@
 <template>
   <div class="jumbotron">
     <div class="card"> 
-      <router-link to="/entities/new" class="btn btn-success">Create New Entity</router-link>
+      <router-link to="/entities/new" class="btn btn-success">
+        <i class="bi bi-plus-circle me-1"></i> Create New Entity
+      </router-link>
     </div>
   </div>
 </template>
