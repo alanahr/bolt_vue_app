ba780ef23fe9c3112b27180ce6117fc399ced812
AAR <25590997+alanahr@users.noreply.github.com>
2025-07-04 00:36:26 -0500

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
 </template>
 
 <script setup lang="ts">
-import AppNavbar from './components/layout/AppNavbar.vue'
-import AppFooter from './components/layout/AppFooter.vue'
-import AlertBar from './components/layout/AlertBar.vue';
+import AppNavbar from './layout/AppNavbar.vue'
+import AppFooter from './layout/AppFooter.vue'
+import AlertBar from './layout/AlertBar.vue';
 </script>
 
 <style>
