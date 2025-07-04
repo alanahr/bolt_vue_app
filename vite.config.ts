3f5e22fc850d8b4761323b29ecf2d7de4f0cff2f
AAR <25590997+alanahr@users.noreply.github.com>
2025-07-04 01:35:28 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   //#todo do API url here?
+  //          __API_URL__: JSON.stringify('http://localhost:8000')
+
   plugins: [vue()],
 })
