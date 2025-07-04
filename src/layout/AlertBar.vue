bc6acfa9e3505e8683e1bc428135fcc38a777c95
AAR <25590997+alanahr@users.noreply.github.com>
2025-07-04 00:25:16 -0500

    Updated AlertBar.vue

---
+++ src/layout/AlertBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <span 
+    v-for="(alert, index) in alerts" 
+    :key="index"
+  >
+  <div>
+    <div class="alert" :class="alert.status" role="alert">
+        <button class="ml-2" @click.prevent="removeAlert(index)">X</button> {{ alert.message }} 
+    </div>
+  </div>
+</span>
+</template>
+
+<script setup lang="ts">
+
+import { computed } from 'vue'
+import { useAlertsStore } from '../../stores/alertStore'
+
+const alertStore = useAlertsStore()
+
+const alerts = computed(() => {
+  return alertStore.alerts
+})
+
+const removeAlert = (index: number) => {
+  alertStore.removeAlert(index)
+}
+
+</script>
