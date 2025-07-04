617504b84ba7a8e30bf6ca31b59a7870aaccd958
AAR <25590997+alanahr@users.noreply.github.com>
2025-07-04 01:08:29 -0500

    Updated Landing.vue

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -6,5 +6,10 @@
         <i class="bi bi-plus-circle me-1"></i> Create New Entity
       </router-link>
     </div>
+     <div class="card"> 
+      <router-link to="/positions/new" class="btn btn-success">
+        <i class="bi bi-plus-circle me-1"></i> Create New Job
+      </router-link>
+    </div>
   </div>
 </template>
